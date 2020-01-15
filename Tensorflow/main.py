6dc313f84bf92245baa984abd9db25ecefe9fa75
neilchattopadhyay123 <neilchattopadhyay123@gmail.com>
2020-01-15 18:19:47 -0500

    added Images folder

--- Tensorflow/main.py
+++ Tensorflow/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from tensorflow import keras
 
-fashion_mnist= keras.datasets.fashion_mnist
+fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
